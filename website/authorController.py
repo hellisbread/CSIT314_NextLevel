4e40e5e8afc236165dcefcf6cf3b2ba6351808ba
Brandon | Laptop <xinomnom@gmail.com>
2022-11-03 20:59:52 +0800

    minor changes to make all codes uniform

--- website/authorController.py
+++ website/authorController.py
@@ -1,10 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import UploadFileForm
 from .models import Author, Paper
-from django.template import loader
-from django.http import HttpResponse, HttpResponseRedirect
+
 from django.contrib import messages
-from django.urls import reverse
 
 def authorPage(request):
     return render(request, 'author/author.html', {'author_id': id})
@@ -22,33 +20,32 @@ def submitPaperPage(request):
         else:
             messages.error(request, "Invalid input found. Please ensure all fields are filled.")
 
-        return HttpResponseRedirect(reverse('viewPaperPage'))
+        return redirect('viewPaperPage')
 
     else:
         form = UploadFileForm()
         authors = Author.getAllAuthor()
+
     return render(request, 'author/submitPaper.html', {'form': form, 'authors': authors})
 
 def viewPaperPage(request):
     papers = Paper.objects.all()
-    template = loader.get_template('author/viewPaper.html')
     context = {'papers' : papers}
 
-    return HttpResponse(template.render(context, request))
+    return render(request, 'author/viewPaper.html', context)
 
 def deleteSubmittedPaper(request, id):
     paper = Paper.objects.get(id=id)
     paper.delete()
-    return HttpResponseRedirect(reverse('viewPaperPage'))
+    return redirect('viewPaperPage')
 
 def readSubmittedPaper(request, id):
     paper = Paper.objects.get(id=id)
-    template = loader.get_template('author/readSubmittedPaper.html')
     text = paper.saved_file.read().decode()
 
     body = {'content': text}
 
-    return HttpResponse(template.render(body, request))
+    return render(request, 'author/readSubmittedPaper.html', body)
 
 def updateSubmittedPaper(request, id):
     if request.method == 'POST':
@@ -66,7 +63,7 @@ def updateSubmittedPaper(request, id):
         else:
             messages.error(request, "Invalid input found. Please ensure all fields are filled.")
 
-        return HttpResponseRedirect(reverse('viewPaperPage'))
+        return redirect('viewPaperPage')
     else:
         form = UploadFileForm()
         authors = Author.getAllAuthor()
