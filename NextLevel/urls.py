f6ebc4777e223ed849955cb8c7ad89b64d80b747
Brandon | Laptop <xinomnom@gmail.com>
2022-11-09 17:10:44 +0800

    reviewer can update max papers

--- NextLevel/urls.py
+++ NextLevel/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path
 
 from website import views, loginController, userController, authorController, systemAdminController, reviewerController, conferenceChairController
+
 urlpatterns = [
     #General URL
     path('', loginController.index, name = 'index'),
@@ -41,6 +42,7 @@ urlpatterns = [
     path('reviewer/bid/', reviewerController.bidPaper, name='bidPaper'),
     path('reviewer/bid/add/<int:id>', reviewerController.addBidPaper, name='addBidPaper'),
     path('reviewer/settings/', reviewerController.settings, name='settings'),
+    path('reviewer/settings/update', reviewerController.updateSettings, name='updateSettings'),
     path('reviewer/papers/', reviewerController.biddedPaperPage, name='biddedPaper'),
     path('reviewer/paper/review/<int:id>',reviewerController.reviewPage,name='reviewPaper'),
     #Conference Chair URL
--- website/reviewerController.py
+++ website/reviewerController.py
@@ -43,7 +43,29 @@ def addBidPaper(request, id):
 
 
 def settings(request):
-    return render(request, 'reviewer/settings.html', {})
+
+    reviewer_id = request.session['ReviewerLogged']
+
+    max_paper = Reviewer.getMaxPaperByID(reviewer_id)
+
+    context = {'max_paper':max_paper}
+
+    return render(request, 'reviewer/settings.html', context)
+
+def updateSettings(request):
+    if(request.POST):
+        reviewer_id = request.session['ReviewerLogged']
+
+        new_maxPaper = request.POST['maxpaper']
+
+        success = Reviewer.setMaxPaperByID(reviewer_id, new_maxPaper)
+
+        if(success):
+            messages.success(request, "Successfully updated max papers.")
+        else:
+            messages.error(request, "Error updating max paper. Please try again.")
+
+        return redirect('settings')
 
 def biddedPaperPage(request):
 
