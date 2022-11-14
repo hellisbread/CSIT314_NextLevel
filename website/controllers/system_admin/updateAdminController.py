51454a4c1dec852ccb5fb02367a6674668b9df98
Brandon | Laptop <xinomnom@gmail.com>
2022-11-14 11:06:01 +0800

    login validation

--- website/controllers/system_admin/updateAdminController.py
+++ website/controllers/system_admin/updateAdminController.py
@@ -8,47 +8,51 @@ from django.core.validators import validate_email
 from website.models import Users, SystemAdmin
 
 def updateAdmins(request,id):
-    if(request.POST):
-        role = request.POST['roleList']
-        username = request.POST['username']
-        password = request.POST['password']
-        email = request.POST['email']
+    if 'SysAdminLogged' in request.session:
+        if(request.POST):
+            role = request.POST['roleList']
+            username = request.POST['username']
+            password = request.POST['password']
+            email = request.POST['email']
 
-        myadmin = SystemAdmin.getSystemAdminByID(id)
+            myadmin = SystemAdmin.getSystemAdminByID(id)
 
-        if(CheckEmailinDB(email) == False and email!=myadmin.email):
-            messages.error(request, "Invalid Email. This email exists in the DB already.")
-            return redirect('update_admin', id=id)
+            if(CheckEmailinDB(email) == False and email!=myadmin.email):
+                messages.error(request, "Invalid Email. This email exists in the DB already.")
+                return redirect('update_admin', id=id)
 
-        if(validateEmail(email) == False and email!=myadmin.email):
-            messages.error(request, "Invalid Email. Please ensure the email is in the valid format.")
-            return redirect('update_admin', id=id)
+            if(validateEmail(email) == False and email!=myadmin.email):
+                messages.error(request, "Invalid Email. Please ensure the email is in the valid format.")
+                return redirect('update_admin', id=id)
 
-        if(CheckUsernameinDB(username) == False and username!=myadmin.username):
-            messages.error(request, "Invalid username. This username exists in the DB already.")
-            return redirect('update_admin', id=id)
+            if(CheckUsernameinDB(username) == False and username!=myadmin.username):
+                messages.error(request, "Invalid username. This username exists in the DB already.")
+                return redirect('update_admin', id=id)
 
-        #Updating
-        if(role=="System Admin"):
-            success = SystemAdmin.UpdateSysAdminByID(id,email,username,password)
+            #Updating
+            if(role=="System Admin"):
+                success = SystemAdmin.UpdateSysAdminByID(id,email,username,password)
+            else:
+                name = request.POST['fullname']
+                maxPaper = request.POST['maxPaper']
+
+                success = SystemAdmin.UpdateRoleByID(id,email,username,password,name,maxPaper,role)
+
+            if(success):
+                messages.success(request, "Successfully updated System Admin ID: " + str(id))
+                return redirect('systemAdminPage')
+            else:
+                messages.error(request, "There was an error updating.")
+                return redirect('update_admin', id=id)
+
         else:
-            name = request.POST['fullname']
-            maxPaper = request.POST['maxPaper']
+            myadmin = SystemAdmin.getSystemAdminByID(id)
+            context = {'admin':myadmin}
 
-            success = SystemAdmin.UpdateRoleByID(id,email,username,password,name,maxPaper,role)
-
-        if(success):
-            messages.success(request, "Successfully updated System Admin ID: " + str(id))
-            return redirect('systemAdminPage')
-        else:
-            messages.error(request, "There was an error updating.")
-            return redirect('update_admin', id=id)
-
+            return render(request, 'admin/update_admin.html', context)
     else:
-        myadmin = SystemAdmin.getSystemAdminByID(id)
-        context = {'admin':myadmin}
-
-        return render(request, 'admin/update_admin.html', context)
+        messages.error(request, "Please login before accessing this page.")
+        return redirect('index')
 
 
 
