67feebb068f7010494ee7836645131e2ee97b4d5
Patricianatasha22 <patricianatasha01@gmail.com>
2022-11-15 13:45:22 +0800

    conference chair front end integration

--- website/controllers/conference_chair/CCreadSubmittedPaperController.py
+++ website/controllers/conference_chair/CCreadSubmittedPaperController.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from ...models import Paper
 
 def readSubmittedPaper(request,id):
+    paper = Paper.getPaper(id)
     text = Paper.readSubmittedPaper(id)
 
-    context = {'content': text}
+    context = {'paper': paper, 'content': text}
 
     return render(request, 'conferenceChair/readSubmittedPaper.html', context)
--- website/controllers/conference_chair/CCviewReviewController.py
+++ website/controllers/conference_chair/CCviewReviewController.py
@@ -7,10 +7,12 @@ from django.contrib import messages
 
 def viewReview(request, id):
     reviews = Review.getAllReviewByPaperID(id)
+    numberOfReview = reviews.count()
+
     if reviews.count() > 0:
         paper = Paper.getPaper(id)
         text = Paper.readSubmittedPaper(id)
-        context = {'reviews' : reviews, 'paper': paper, 'content':text}
+        context = {'reviews' : reviews, 'numberOfReview' : numberOfReview, 'paper': paper, 'content':text}
 
         return render(request, 'conferenceChair/viewReview.html', context)
     else:
