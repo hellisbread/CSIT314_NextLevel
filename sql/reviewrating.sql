2b5f4e4b5e0bbf3407a8af3d44dccfdc32c2f1d3
winaKim <115201240+winaKim@users.noreply.github.com>
2022-11-16 02:24:51 +0800

    Update reviewrating.sql

--- sql/reviewrating.sql
+++ sql/reviewrating.sql
@@ -4,6 +4,7 @@ CREATE TABLE [IF NOT EXISTS] reviewrating (
 	author_id INTEGER,
 	paper_id INTEGER,
 	reviewer_id INTEGER,
+	review_id INTEGER,
 	FOREIGN KEY (paper_id)
 		REFERENCES paper(id)
 		ON DELETE CASCADE
@@ -12,14 +13,20 @@ CREATE TABLE [IF NOT EXISTS] reviewrating (
 		REFERENCES reviewer(id)
 		ON DELETE CASCADE
 		ON UPDATE NO ACTION,
+	FOREIGN KEY (review_id )
+		REFERENCES review(id)
+		ON DELETE CASCADE
+		ON UPDATE NO ACTION
 );
 
-INSERT INTO reviewrating (id, rating, author_id, paper_id, reviewer_id) VALUES 
-(1, -1, 3, 2, 27),
-(2, 0, 2, 2, 40),
-(3, 3, 9, 9, 37),
-(4, -1, 3, 2, 27),
-(5, 3, 20, 19, 44),
-(6, -1, 5, 4, 30),
-(7, 2, 12, 11, 36),
-(8, 2, 17, 16, 41);
+
+INSERT INTO reviewrating (id, rating, author_id, paper_id, reviewer_id, review_id) VALUES 
+(1, -1, 2, 2, 27, 1),
+(2, 0, 2, 2, 40, 2),
+(3, 3, 9, 9, 34, 5),
+(4, -1, 3, 2, 27, 1),
+(5, 3, 20, 19, 44, 9),
+(6, -1, 5, 4, 30, 4),
+(7, 2, 12, 11, 36, 6),
+(8, 2, 17, 16, 41, 8);
+
